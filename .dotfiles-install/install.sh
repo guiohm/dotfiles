e73cba3de8a1f0db40931cf933ccc986423f3de1
Guilhaume Bordiau <github@guilhaume.fr>
2020-11-03 17:45:51 +0100

    fix backup

--- .dotfiles-install/install.sh
+++ .dotfiles-install/install.sh
@@ -8,7 +8,7 @@ if [ $? = 0 ]; then
     echo "Checked out config.";
 else
     echo "Backing up pre-existing dot files.";
-    config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} |  xargs sh -c 'for arg do mkdir -p .config-backup/`dirname "$arg"`; mv "$arg" .config-backup/"$arg"; done'
+    config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} |  xargs sh -c 'for arg do mkdir -p .config-backup/`dirname "$arg"`; mv "$arg" .config-backup/"$arg"; done' _
 fi;
 config checkout
 config config status.showUntrackedFiles no
