3a28ff89254ccde9858735b6f0d07a38865c5527
Guilhaume Bordiau <guilhaume.bordiau@leul.fr>
2022-04-12 21:31:16 +0200

    update install script

--- .dotfiles-install/install.sh
+++ .dotfiles-install/install.sh
@@ -1,17 +1,17 @@
+#!/bin/sh
+
 git clone --bare git@github.com:guiohm/myWork_WSL_DotFiles.git "$HOME/.dotfiles-repo"
 function config {
    git --git-dir="$HOME/.dotfiles-repo/" --work-tree="$HOME" $@
 }
+
+echo "Backing up pre-existing dot files.";
 mkdir -p .config-backup
+config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs sh -c 'for arg do mkdir -p .config-backup/`dirname "$arg"`; mv "$arg" .config-backup/"$arg"; done' _
+
 config checkout
-if [ $? = 0 ]; then
-    echo "Checked out config.";
-else
-    echo "Backing up pre-existing dot files.";
-    config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} |  xargs sh -c 'for arg do mkdir -p .config-backup/`dirname "$arg"`; mv "$arg" .config-backup/"$arg"; done' _
-fi;
-config checkout
+config config core.eol lf
+config config core.autocrlf false
 config config status.showUntrackedFiles no
 config submodule init
 config submodule update
-source $HOME/.bashrc
