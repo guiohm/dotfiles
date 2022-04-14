ae189f2c3c9650b88ca09e4bddf9c0f9c7d4edf9
Guilhaume Bordiau <github@guilhaume.fr>
2022-04-14 15:03:52 +0200

    adds winget installs for future reference

---
+++ .dotfiles-install/winget_install.ps1
@@ -0,0 +1,52 @@
+
+# vscode has to be installed interactively to add shell extensions (open with...)
+winget install -i -e --id Microsoft.VisualStudioCode.Insiders
+winget install -e --id Microsoft.WindowsTerminal
+
+# Fix win 11 UI
+winget install -e --id Microsoft.PowerToys
+winget install -e --id valinet.ExplorerPatcher
+
+winget install -e --id OpenVPNTechnologies.OpenVPN
+
+winget install -e --id Microsoft.Teams.Exploration
+winget install -e --id Discord.Discord
+
+winget install -e --id Canonical.Ubuntu
+winget install -e --id eloston.ungoogled-chromium
+winget install -e --id Git.Git
+winget install -e --id Microsoft.PowerShell
+
+
+winget install -e --id Microsoft.VC++2015-2022Redist-x64
+winget install -e --id Microsoft.dotnet
+winget install -e --id Microsoft.OpenJDK.17
+winget install -e --id Python.Python.3
+winget install -e --id JetBrains.Toolbox
+#winget install -e --id Microsoft.VisualStudio.2022.Community
+
+# vscode has to be installed interactively to add shell extensionis (open with...)
+winget install -i -e --id Microsoft.VisualStudioCode.Insiders
+
+winget install -i -e --id Microsoft.SQLServer.2019.Developer
+winget install -i -e --id Microsoft.SQLServerManagementStudio
+winget install -e --id dbeaver.dbeaver
+
+# utilitaires
+winget install -e --id M2Team.NanaZip.Preview
+winget install -e --id TimKosse.FileZilla.Client
+winget install -e --id AntibodySoftware.WizTree
+winget install -e --id KeePassXCTeam.KeePassXC
+winget install -e --id Klocman.BulkCrapUninstaller
+winget install -e --id SublimeHQ.SublimeMerge
+
+# sanbox/perso
+winget install -e --id MKProj.Minerva
+winget install -e --id MITMediaLab.Scratch.3
+winget install -i -e --id VideoLAN.VLC
+winget install -e --id Streamlink.Streamlink
+winget install -e --id Spotify.Spotify
+
+## pas dans winget
+
+https://github.com/ChangemakerStudios/Papercut-SMTP
